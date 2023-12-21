842a6f5fc299a9c351d4666703f62f7532a7e83e
Buran-s <ssenozibrandon05@gmail.com>
2023-12-21 18:03:54 +0300

    backend initials

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,57 @@
+const expressAsyncHandler = require("express-async-handler")
+const bcrypt = require('bcryptjs')
+
+
+const User = require('../models/userModel')
+// register a new user
+// router /api/users
+// access public
+const registerUser = expressAsyncHandler(async (req,res)=>{
+   const {name,email,password} = req.body
+
+  //  Validation
+  if(!name || !email || !password){
+    res.status(400)
+    throw new Error ('Please include all fields')
+  }
+
+  // find if user already exists
+  const userExists = await User.findOne({email})
+  if(userExists){
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password,salt)
+
+  // create user
+  const user = User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  }else{
+    res.status(400)
+    throw new Error('Invalid User data')
+  } 
+})
+
+// login a new user
+// router /api/users/login
+// access public
+const loginUser = expressAsyncHandler(async (req,res)=>{
+  res.send('login Route')
+})
+
+module.exports = {
+  registerUser,
+  loginUser
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('server...')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleWare')
+const connectDB  = require('./config/db')
+const PORT = process.env.PORT || 8000
+
+// connection to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(errorHandler)
+app.use('/api/users', require('./routes/UserRoutes'))
+app.use(express.urlencoded({extended: false}))
+
+app.listen(PORT,()=>console.log(`server started on port ${PORT}`))
