f59af5fe3d49f4a604cad35b24b7d57735987acb
Buran-s <ssenozibrandon05@gmail.com>
2023-12-31 13:32:57 +0300

    serving frontend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -15,6 +15,19 @@ app.use(express.json())
 app.use(errorHandler)
 app.use('/api/users', require('./routes/UserRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// serve frontend
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,"../frontend/build/")))
+
+  app.get('*',(req,res)=> res.sendFile(__dirname,'../', 'frontend','build','index.html'))
+
+}else{
+  app.get('/',(req,res)=>{
+    res.status(200).json({message: 'Welcome to the support desk'})
+  })
+}
+
 app.use(express.urlencoded({extended: false}))
 
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`))
